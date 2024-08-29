936ab372cfbdf0d8fafd45327a6f7a1aef6bfc4e
Claudio Salazar <contacto@claudiosalazar.cl>
2024-08-29 19:52:34 -0400

    Ajustes graficos para lista de trayectoria

--- src/app/trayectoria/page.tsx
+++ src/app/trayectoria/page.tsx
@@ -4,25 +4,143 @@ function Trayectoria() {
       <div className="col-5 offset-1">
         <h1 className="my-5">Trayectoria</h1>
         <p>Tengo más de 15 años de experiencia profesional en proyectos web. Desde 2017, he participado activamente en proyectos de transformación digital dentro de equipos multidisciplinarios, siempre trabajando bajo metodologías ágiles. En los últimos 7 años, he colaborado en proyectos para importantes entidades bancarias nacionales como Banco Estado, BCI e Itaú Chile. A lo largo de mi carrera, mi enfoque ha estado siempre centrado en la mejora continua de mis conocimientos en las últimas tecnologías para el desarrollo frontend.</p>
-
-        <a  className="botonCv contenedor-btn-cv my-4 mt-lg-5 mt-xl-5">
-                <p className="ms-md-5 ms-lg-4 ms-xl-0">
-                  <a href="/claudio-salazar-cv2024.pdf" target="_blank" className="curriculum d-flex align-items-center">
-                    <span className="material-symbols-outlined me-2">
-                      picture_as_pdf
-                    </span>
-                    <span>Descargar CV</span>
-                  </a>
-                </p>
-              </a>
+        <a className="botonCv contenedor-btn-cv my-4 mt-lg-5 mt-xl-5">
+          <p className="ms-md-5 ms-lg-4 ms-xl-0 resumen">
+            <a href="/claudio-salazar-cv2024.pdf" target="_blank" className="curriculum d-flex align-items-center">
+              <span className="material-symbols-outlined me-2">
+                picture_as_pdf
+              </span>
+              <span>Descargar CV</span>
+            </a>
+          </p>
+        </a>
       </div>
       <div className="col-5">
         <ul>
-          <li>asdas</li>
-          <li>asdas</li>
-          <li>asdas</li>
-          <li>asdas</li>
-          <li>asdas</li>
+          <li>
+            <div className="d-flex justify-content-between">
+              <span className="d-block">
+                <p className="mb-1">
+                  <strong className='d-flex align-items-center nombre-trabajo'>
+                    <span className='me-1'>ITAU</span>
+                    <span className="fondo-strong" />
+                  </strong>
+                </p>
+                <small>UI Developer</small>
+              </span>
+              <span className="duracion-trabajo text-end d-block">
+                <p className="anho-trabajo">
+                  2021 - 2023
+                  <span className="fondo-strong" />
+                </p>
+              </span>
+            </div>
+            <span className="fondo-linea-trabajo" />
+          </li>
+          
+          <li>
+            <div className="d-flex justify-content-between">
+              <span className="d-block">
+                <p className="mb-1">
+                  <strong className='d-flex align-items-center nombre-trabajo'>
+                    <span className='me-1'>everis / NTT DATA</span>
+                    <span className="fondo-strong" />
+                  </strong>
+                </p>
+                <small>Prototyper</small>
+              </span>
+              <span className="duracion-trabajo">
+                <p className="anho-trabajo">
+                  2017 - 2021
+                  <span className="fondo-strong" />
+                </p>
+              </span>
+            </div>
+            <span className="fondo-linea-trabajo" />
+          </li>
+
+          <li>
+            <div className="d-flex justify-content-between">
+              <span className="d-block">
+                <p className="mb-1">
+                  <strong className='d-flex align-items-center nombre-trabajo'>
+                    <span className='me-1'>BBK Group</span>
+                    <span className="fondo-strong" />
+                  </strong>
+                </p>
+                <small>Frontend / UX</small>
+              </span>
+              <span className="duracion-trabajo">
+                <p className="anho-trabajo">
+                  2017
+                  <span className="fondo-strong" />
+                </p>
+              </span>
+            </div>
+            <span className="fondo-linea-trabajo" />
+          </li>
+          <li>
+            <div className="d-flex justify-content-between">
+              <span className="d-block">
+                <p className="mb-1">
+                  <strong className='d-flex align-items-center nombre-trabajo'>
+                    <span className='me-1'> MAYO Group</span>
+                    <span className="fondo-strong" />
+                  </strong>
+                </p>
+                <small>Diseñador Frontend / UI / Productor Digital / Lider Equipo Producción Digital</small>
+              </span>
+              <span className="duracion-trabajo">
+                <p className="anho-trabajo">
+                  2009 - 2016
+                  <span className="fondo-strong" />
+                </p>
+              </span>
+            </div>
+            <span className="fondo-linea-trabajo" />
+          </li>
+
+          <li>
+            <div className="d-flex justify-content-between">
+              <span className="d-block">
+                <p className="mb-1">
+                  <strong className='d-flex align-items-center nombre-trabajo'>
+                    <span className='me-1'>KITEKNOLOGY</span>
+                    <span className="fondo-strong" />
+                  </strong>
+                </p>
+                <small>Diseñador Web Senior</small>
+              </span>
+              <span className="duracion-trabajo">
+                <p className="anho-trabajo">
+                  2008 - 2009
+                  <span className="fondo-strong" />
+                </p>
+              </span>
+            </div>
+            <span className="fondo-linea-trabajo" />
+          </li>
+
+          <li>
+            <div className="d-flex justify-content-between">
+              <span className="d-block">
+                <p className="mb-1">
+                  <strong className='d-flex align-items-center nombre-trabajo'>
+                    <span className='me-1'>NUESTRAGENCIA</span>
+                    <span className="fondo-strong" />
+                  </strong>
+                </p>
+                <small>Diseñador Gráfico / Diseñador Web / Frontend</small>
+              </span>
+              <span className="duracion-trabajo">
+                <p className="anho-trabajo">
+                  2004 - 2007
+                  <span className="fondo-strong" />
+                </p>
+              </span>
+            </div>
+            <span className="fondo-linea-trabajo" />
+          </li>
         </ul>
       </div>
 
