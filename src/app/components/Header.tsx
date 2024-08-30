2779624c735b32f1b84ef324aceebdaeabf3276b
Claudio Salazar <contacto@claudiosalazar.cl>
2024-08-29 23:38:21 -0400

    Ajustes de estilos a menu

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -6,17 +6,18 @@ import { animated, useSpring } from '@react-spring/web';
 import Link from "next/link";
 
 function Header() {
+  const pathname = usePathname();
   const [menuPosicion, setPosicion] = useState<string>('top-menu');
   const [menuColor, setColor] = useState<string>('link-inicio');
 
-  const links = [
-    { href: "/", label: "Inicio" },
-    { href: "/portafolio", label: "Portafolio" },
-    { href: "/conocimientos", label: "Conocimientos" },
-    { href: "/trayectoria", label: "Trayectoria" },
-  ];
-
-  const pathname = usePathname();
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      const savedMenuPosicion = localStorage.getItem('menuPosicion');
+      const savedMenuColor = localStorage.getItem('menuColor');
+      if (savedMenuPosicion) setPosicion(savedMenuPosicion);
+      if (savedMenuColor) setColor(savedMenuColor);
+    }
+  }, []);
 
   const updateMenuState = (path: string) => {
     let nextPosicion = '';
@@ -38,14 +39,27 @@ function Header() {
 
     setPosicion(nextPosicion);
     setColor(nextColor);
+    if (typeof window !== 'undefined') {
+      localStorage.setItem('menuPosicion', nextPosicion);
+      localStorage.setItem('menuColor', nextColor);
+    }
   };
 
   useEffect(() => {
     updateMenuState(pathname);
   }, [pathname]);
 
-  const handleClick = (name: string) => {
-    updateMenuState(links.find(link => link.label === name)?.href || '/');
+  const initialAnimationState = () => {
+    if (menuPosicion === 'top-menu') {
+      return { top: '0%', bottom: '100%', transform: 'translateY(0%)' };
+    } else if (menuPosicion === 'center-menu') {
+      return { top: '33%', bottom: '67%', transform: 'translateY(-33%)' };
+    } else if (menuPosicion === 'mid-bottom-menu') {
+      return { top: '67%', bottom: '33%', transform: 'translateY(-67%)' };
+    } else if (menuPosicion === 'bottom-menu') {
+      return { top: '100%', bottom: '0%', transform: 'translateY(-100%)' };
+    }
+    return { top: '0%', bottom: '100%', transform: 'translateY(0%)' };
   };
 
   const animation = useSpring({
@@ -60,7 +74,7 @@ function Header() {
         await next({ top: '100%', bottom: '0%', transform: 'translateY(-100%)' });
       }
     },
-    from: { top: '0%', bottom: '100%', transform: 'translateY(0%)' },
+    from: initialAnimationState(),
     config: { duration: 500 },
   });
 
@@ -68,13 +82,30 @@ function Header() {
     <header>
       <nav>
         <animated.ul style={animation} className={`menu ${menuPosicion}`}>
-          {links.map((link) => (
-            <li className="nav-item" key={link.label}>
-              <Link href={link.href} className={`nav-link ${pathname === link.href ? "active" : ""} ${menuColor}`} onClick={() => handleClick(link.label)}>
-                {link.label}
-              </Link>
-            </li>
-          ))}
+          <li className="nav-item">
+            <Link href="/" className={`nav-link ${pathname === '/' ? "active" : ""} ${menuColor}`}>
+            <span className="indicador-boton"></span>
+              Inicio
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link href="/portafolio" className={`nav-link ${pathname === '/portafolio' ? "active" : ""} ${menuColor}`}>
+              <span className="indicador-boton"></span>
+              Portafolio
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link href="/conocimientos" className={`nav-link ${pathname === '/conocimientos' ? "active" : ""} ${menuColor}`}>
+              <span className="indicador-boton"></span>
+              Conocimientos
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link href="/trayectoria" className={`nav-link ${pathname === '/trayectoria' ? "active" : ""} ${menuColor}`}>
+              <span className="indicador-boton"></span>
+              Trayectoria
+            </Link>
+          </li>
         </animated.ul>
       </nav>
     </header>
