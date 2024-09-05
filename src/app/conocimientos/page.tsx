bf167d89bb2df8e17abb9b8d96b771de13bca7a7
Claudio Salazar <contacto@claudiosalazar.cl>
2024-09-04 21:28:23 -0400

    Ajuste a accordion version desktop

--- src/app/conocimientos/page.tsx
+++ src/app/conocimientos/page.tsx
@@ -77,7 +77,7 @@ function Conocimientos() {
 
   return (
     <section className="info-conocimientos conocimientos row d-flex align-items-center">
-      <div className="col-6 mx-auto">
+      <div className="col-6 mx-auto py-5">
         <h1 className="my-5">Conocimientos</h1>
         
         <div className="accordion" id="accordionFlushExample">
@@ -85,7 +85,7 @@ function Conocimientos() {
             <h2 className="accordion-header">
               <button className="accordion-button collapsed" onClick={handleClick1}  type="button" data-bs-toggle="collapse" data-bs-target="#panelsAbierto-conocimientoUno" aria-expanded="true" aria-controls="panelsAbierto-conocimientoUno">
                 <div className="icono-item">
-                  <div className={`vertical`}></div>
+                  <div className={`vertical ${isOpen1 ? 'item-abierto' : ''}`}></div>
                   <div className="horizontal"></div>
                 </div>
                 <div className={`iconos d-none d-md-flex ${isOpen1 ? 'active' : ''}`}>
@@ -102,19 +102,15 @@ function Conocimientos() {
                 <p>
                   Con más de 15 años de experiencia en desarrollo web, domino <strong>HTML5</strong> semántico y <strong>CSS3</strong> para crear proyectos responsivos. Utilizo <strong>preprocesadores CSS</strong> y <strong>Bootstrap (4 y versiones superiores)</strong> para optimizar el flujo de trabajo y garantizar interfaces consistentes en diversos dispositivos.
                 </p>
+                <hr className='separador-conocimientos' />
               </div>
             </div>
           </div>
-
-
-
-
-
           <div className="accordion-item">
             <h2 className="accordion-header">
             <button className="accordion-button collapsed" onClick={handleClick2} type="button" data-bs-toggle="collapse" data-bs-target="#panelsAbierto-conocimientoDos" aria-expanded="true" aria-controls="panelsAbierto-conocimientoDos">
               <div className="icono-item">
-                <div className={`vertical`}></div>
+                <div className={`vertical ${isOpen2 ? 'item-abierto' : ''}`}></div>
                 <div className="horizontal"></div>
               </div>
               <div className={`iconos d-none d-md-flex ${isOpen2 ? 'active' : ''}`}>
@@ -130,6 +126,7 @@ function Conocimientos() {
             <div id="panelsAbierto-conocimientoDos" className="accordion-collapse collapse">
               <div className="accordion-body">
                 <p>Tengo conocimientos en el desarrollo de proyectos web utilizando <strong>JavaScript</strong>, <strong>PHP</strong> y <strong>NodeJs</strong>. Además, tengo experiencia en la integración y <strong>consumo de datos provenientes de APIs</strong>, así como en el manejo de bases de datos  <strong>MySQL</strong>, lo que me permite crear aplicaciones dinámicas y conectadas a servicios externos de manera eficiente.</p>
+                <hr className='separador-conocimientos' />
               </div>
             </div>
           </div>
@@ -137,7 +134,7 @@ function Conocimientos() {
             <h2 className="accordion-header">
               <button className="accordion-button collapsed" onClick={handleClick3} type="button" data-bs-toggle="collapse" data-bs-target="#panelsAbierto-conocimientoTres" aria-expanded="false" aria-controls="panelsAbierto-conocimientoTres">
                 <div className="icono-item">
-                  <div className={`vertical`}></div>
+                  <div className={`vertical ${isOpen3 ? 'item-abierto' : ''}`}></div>
                   <div className="horizontal"></div>
                 </div>
                 <div className={`iconos d-none d-md-flex ${isOpen3 ? 'active' : ''}`}>
@@ -152,6 +149,7 @@ function Conocimientos() {
             <div id="panelsAbierto-conocimientoTres" className="accordion-collapse collapse">
               <div className="accordion-body">
                 <p>Poseo experiencia sólida en el desarrollo de proyectos web en frameworks como <strong>Angular (15 y versiones superiores)</strong>, <strong>React (18)</strong> y <strong>WordPress</strong>. Además, tengo conocimientos en el manejo de dependencias a través de <strong>NPM</strong>, lo que me permite construir aplicaciones robustas y escalables eficientemente. También tengo experiencia en la <strong>Modularización de proyectos</strong>.</p>
+                <hr className='separador-conocimientos' />
               </div>
             </div>
           </div>
@@ -159,7 +157,7 @@ function Conocimientos() {
             <h2 className="accordion-header">
               <button className="accordion-button collapsed" onClick={handleClick4} type="button" data-bs-toggle="collapse" data-bs-target="#panelsAbierto-conocimientoCuatro" aria-expanded="false" aria-controls="panelsAbierto-conocimientoCuatro">
                 <div className="icono-item">
-                  <div className={`vertical`}></div>
+                  <div className={`vertical ${isOpen4 ? 'item-abierto' : ''}`}></div>
                   <div className="horizontal"></div>
                 </div>
                 <div className={`iconos d-none d-md-flex ${isOpen4 ? 'active' : ''}`}>
@@ -174,6 +172,7 @@ function Conocimientos() {
             <div id="panelsAbierto-conocimientoCuatro" className="accordion-collapse collapse">
               <div className="accordion-body">
                 <p>Tengo experiencia sólida en el manejo de repositorios utilizando <strong>Git</strong>, lo que me permite gestionar eficientemente el control de versiones en proyectos de desarrollo. Además, cuento con habilidades profesionales en el uso de herramientas de gestión de proyectos como <strong>Atlassian</strong>, <strong>GitHub</strong> y <strong>GitLab</strong>, lo que facilita la colaboración y el seguimiento de tareas en equipos de trabajo distribuidos.</p>
+                <hr className='separador-conocimientos' />
               </div>
             </div>
           </div>
@@ -181,7 +180,7 @@ function Conocimientos() {
             <h2 className="accordion-header">
               <button className="accordion-button collapsed" onClick={handleClick5} type="button" data-bs-toggle="collapse" data-bs-target="#panelsAbierto-conocimientoCinco" aria-expanded="false" aria-controls="panelsAbierto-conocimientoCinco">
                 <div className="icono-item">
-                  <div className={`vertical`}></div>
+                  <div className={`vertical ${isOpen5 ? 'item-abierto' : ''}`}></div>
                   <div className="horizontal"></div>
                 </div>
                 <div className={`iconos d-none d-md-flex ${isOpen5 ? 'active' : ''}`}>
@@ -198,6 +197,7 @@ function Conocimientos() {
             <div id="panelsAbierto-conocimientoCinco" className="accordion-collapse collapse">
               <div className="accordion-body">
                 <p>Tengo un dominio profesional en el uso de herramientas de diseño UI como <strong>Figma</strong>, <strong>Sketch</strong> y <strong>Adobe XD</strong>. Con más de 15 años de experiencia, también tengo un amplio conocimiento en el uso de software de diseño y edición de imágenes de la suite <strong>Creative Cloud</strong> de <strong>Adobe</strong>, lo que me permite crear y desarrollar proyectos visuales de alta calidad y precisión.</p>
+                <hr className='separador-conocimientos' />
               </div>
             </div>
           </div>
@@ -205,7 +205,7 @@ function Conocimientos() {
             <h2 className="accordion-header">
               <button className="accordion-button collapsed" onClick={handleClick6} type="button" data-bs-toggle="collapse" data-bs-target="#panelsAbierto-conocimientoSeis" aria-expanded="false" aria-controls="panelsAbierto-conocimientoSeis">
                 <div className="icono-item">
-                  <div className={`vertical`}></div>
+                  <div className={`vertical ${isOpen6 ? 'item-abierto' : ''}`}></div>
                   <div className="horizontal"></div>
                 </div>
                 <div className={`iconos d-none d-md-flex ${isOpen6 ? 'active' : ''}`}>
@@ -223,7 +223,6 @@ function Conocimientos() {
           </div>
         </div>
       </div>
-
     </section>
   );
 }
