ecdfdaed5b1328ba455fe16cfe65492f8a8b83ad
Claudio Salazar <contacto@claudiosalazar.cl>
2024-09-07 16:30:50 -0400

    Logica de componente contacteme

--- src/app/contactame/page.tsx
+++ src/app/contactame/page.tsx
@@ -1,9 +1,47 @@
-function Contactame () {
+'use client';
+
+import React, { useState } from 'react';
+import { motion } from 'framer-motion';
+
+function Contactame() {
+  const [isActive, setIsActive] = useState(false);
+
+  const handleClick = () => {
+    setIsActive(!isActive);
+  };
+
   return (
-    <div className="contactame">
-      <a href='#' className="material-symbols-outlined"></a>
-    </div>
-  )
+    <>
+      <div className="contactame">
+        <a href="#" className="material-symbols-outlined" onClick={handleClick}></a>
+      </div>
+      
+      <motion.div initial={{ opacity: 0 }} animate={{ opacity: isActive ? 1 : 0 }} transition={{ duration: 0.35 }} className={isActive ? "contactamen-activo" : "contactamen-oculto"}>
+        <div className='contenido-contactame'>
+          <span className='titulo'>
+            Contáctame
+          </span>
+          <span className='subtitulo my-2 my-md-3'>
+            !Hablemos!
+          </span>
+          <p className='parrafo'>
+            Envíame un mensaje a WhatsApp, contáctame en LinkedIn o si prefieres envíame un email a mi correo personal.
+          </p>
+          <ul className='links d-flex justify-content-center mt-4'>
+            <li>
+              <a href="https://wa.me/56981742311" target="new" className="whatsapp"></a>
+            </li>
+            <li>
+              <a href="https://www.linkedin.com/in/claudiosv/" target="new" className="linkedin"></a>
+            </li>
+            <li>
+              <a href="mailto:contacto@claudiosalazar.cl" target="new" className="email"></a>
+            </li>
+          </ul>
+        </div>
+      </motion.div>
+    </>
+  );
 }
 
 export default Contactame;
