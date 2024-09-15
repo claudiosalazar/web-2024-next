f5fbc082000334bdffade4ecc60185ce97d66374
Claudio Salazar <contacto@claudiosalazar.cl>
2024-09-15 13:25:59 -0300

    Animacion a contenido de footer

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,30 +1,159 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { easeOut, motion, useAnimation } from "framer-motion";
 
-function Footer (){
-  
+function Footer() {
+  const controlNombre = useAnimation();
+  const controlNota = useAnimation();
+  const controlSeparador = useAnimation();
+  const controlLista1 = useAnimation();
+  const controlLista2 = useAnimation();
+  const controlLista3 = useAnimation();
+  const controlLista4 = useAnimation();
+
+  const nombre = {
+    hidden: {
+      y: -10,
+      opacity: 0
+    },
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        easeOut,
+        duration: 0.1
+      }
+    },
+  };
+
+  const nota = {
+    hidden: {
+      y: -10,
+      opacity: 0
+    },
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        easeOut,
+        duration: 0.1
+      }
+    },
+  };
+
+  const separador = {
+    hidden: {
+      opacity: 0,
+      scaleX: 0
+    },
+    visible: {
+      opacity: 0.35,
+      scaleX: 0.45,
+      transition: {
+        easeOut,
+        duration: 0.25
+      }
+    },
+  };
+
+  const lista1 = {
+    hidden: {
+      y: -10,
+      opacity: 0
+    },
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        easeOut,
+        duration: 0.1
+      }
+    },
+  };
+
+  const lista2 = {
+    hidden: {
+      y: -10,
+      opacity: 0
+    },
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        easeOut,
+        duration: 0.1
+      }
+    },
+  };
+
+  const lista3 = {
+    hidden: {
+      y: -10,
+      opacity: 0
+    },
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        easeOut,
+        duration: 0.1
+      }
+    },
+  };
+
+  const lista4 = {
+    hidden: {
+      y: -10,
+      opacity: 0
+    },
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        easeOut,
+        duration: 0.1
+      }
+    },
+  };
+
+  useEffect(() => {
+    const sequence = async () => {
+      await controlNombre.start("visible");
+      await controlNota.start("visible");
+      await controlLista1.start("visible");
+      await controlLista2.start("visible");
+      await controlLista3.start("visible");
+      await controlLista4.start("visible");
+      await controlSeparador.start("visible");
+    };
+
+    sequence();
+  }, [controlNombre, controlNota, controlLista1, controlLista2, controlLista3, controlLista4, controlSeparador]);
+
   return (
     <footer>
-      <p>Claudio Salazar © 2004 - 2024 | V.2</p>
-      <p>
+      <motion.p variants={nombre} animate={controlNombre} initial="hidden" className='nombre'>
+        Claudio Salazar © 2004 - 2024 | V.2
+      </motion.p>
+      <motion.p variants={nota} animate={controlNota} initial="hidden">
         <small>Sitio desarrollado con Next.js, React y Bootstrap</small>
-      </p>
-      <hr />
+      </motion.p>
+      <motion.hr variants={separador} animate={controlSeparador} initial="hidden" className='separador' />
       <ul>
-        <li>
+        <motion.li variants={lista1} animate={controlLista1} initial="hidden">
           <a href="https://www.linkedin.com/in/claudiosv/" target="new" className="btn-footer linkedin" title="Mi perfil en LinkedIn"></a>
-        </li>
-        <li>
+        </motion.li>
+        <motion.li variants={lista2} animate={controlLista2} initial="hidden">
           <a href="https://github.com/claudiosalazar" target="new" className="btn-footer github" title="Mi repositorio en GitHub"></a>
-        </li>
-        <li>
+        </motion.li>
+        <motion.li variants={lista3} animate={controlLista3} initial="hidden">
           <a href="https://www.instagram.com/claudiosvcl/" target="new" className="btn-footer instagram" title="Mi perfil en Intagram"></a>
-        </li>
-        <li>
+        </motion.li>
+        <motion.li variants={lista4} animate={controlLista4} initial="hidden">
           <a href="https://www.behance.net/claudiosalazarcl" target="new" className="btn-footer behance" title="Mi portafolop en Behance"></a>
-        </li>
+        </motion.li>
       </ul>
     </footer>
   );
-};
+}
 
 export default Footer;
--- src/app/template.tsx
+++ src/app/template.tsx
@@ -9,13 +9,15 @@ export default function Template ({
   children: React.ReactNode;
 }) {
   return (
-    <motion.div
-    initial={{ y: -30, scale: 1.01 }}
-    animate={{ y: 0, scale: 1 }}
-    transition={{ duration: 0.3, ease: "easeOut" }}
-    >
-      {children}
-      <Footer />
-    </motion.div>
+    <>
+      <motion.div
+        /*initial={{ y: -30, scale: 1.01 }}
+        animate={{ y: 0, scale: 1 }}*/ initial={{ y: -20 }} animate={{ y: 0 }} transition={{ duration: 0.3, ease: "easeOut" }}>
+        {children}
+      </motion.div>
+      <motion.div initial={{ y: 10, opacity: 0 }} animate={{ y: 0, opacity: 1 }} transition={{ duration: 0.3, ease: "easeOut" }}>
+        <Footer />
+      </motion.div>
+    </>
   )
 }
