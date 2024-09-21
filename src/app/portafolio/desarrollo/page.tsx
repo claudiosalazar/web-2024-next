59e0c35d352a4c64c0528d6a0feab5b347cce126
Claudio Salazar <contacto@claudiosalazar.cl>
2024-09-21 18:23:03 -0300

    Ajuste a diseño para todos los tipos de trabajos en componente portafolcio

--- src/app/portafolio/desarrollo/page.tsx
+++ src/app/portafolio/desarrollo/page.tsx
@@ -71,203 +71,121 @@ import icoIllustrator from '../../assets/iconos/ico-illustrator.png';
   }, [controlTitulo, controlContenido]);
   
   return (
-    <section className="portafolio row d-flex align-items-center">
-      <motion.div variants={titulo} initial='hidden' animate={controlTitulo} className='titulo col-11 ol-xl-10 mx-auto text-center pt-4 d-flex flex-column flex-md-row align-items-md-center'>
-        <h1 className='text-center text-md-end mb-4'>
-          <Link href='/portafolio' className="titulo-seccion d-flex align-items-center justify-content-center justify-content-md-end mb-3 mb-md-0">
-            <span className="ico-atras d-block"></span>
-            <span className=" d-block">Portafolio</span>
-          </Link>
-          <span className="subtitulo-seccion">Desarrollo</span>
-        </h1>
-        <div className="detalle-seccion">
-          <p className="parrafo-seccion">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ullamcorper erat eu ipsum bibendum cursus. Sed mauris arcu, tincidunt ut egestas sit amet, finibus a lacus. Morbi venenatis, tellus ac pharetra tempor, neque quam lacinia dolor, et luctus tellus dui ac urna.
-          </p>
-        </div>
-      </motion.div>
-
-      <motion.div variants={contenidoPortafolio} initial='hidden' animate={controlContenido} className='contenidoPortafolio col-11 mx-auto mt-5'>
-        <div className="row">
-          <div className="col-12 detalle-proyecto">
-            <div className="row">
-              <div className="col-12 col-md-4">
-                <Image src={miBanco1} alt="Mi Banco" className="img-fluid" />
-              </div>
-              <div className="col-12 col-md-8">
-                <h2>Mi Banco</h2>
-                <p>
-                  Proyecto personal de sitio bancario ficticio. Creación de diseño UI y desarrollo frontend y backend (Node.js). En el se pueden realizar transferencias, pagos de productos y modificación de datos del usuario con se ingresa al sitio. Los datos para poder ingresar y visualizar el proyecto son:
-                </p>
-                <p>
-                  <span>Usuario: <strong>12.840.432-5</strong></span>
-                  <br/>
-                  <span>Clave: <strong>Usuario-123</strong></span>
-                </p>
-                <p>
-                  <span>Usuario: <strong>9.328.275-2</strong></span>
-                  <br/>
-                  <span>Clave: <strong>Usuario-123</strong></span>
-                </p>
-                <p>Tecnologías y software utilizados:</p>
-                <ul className="tecnologias">
-                  <li>
-                    <Image src={icoAngular} alt="Angular" />
-                  </li>
-                  <li>
-                    <Image src={icoNode} alt="Nodejs" />
-                  </li>
-                  <li>
-                    <Image src={icoMysql} alt="MySQL" />
-                  </li>
-                  <li>
-                    <Image src={icoHtml} alt="HTML" />
-                  </li>
-                  <li>
-                    <Image src={icoSass} alt="Sass" />
-                  </li>
-                  <li>
-                    <Image src={icoBootstrap} alt="Bootstrap 5" />
-                  </li>
-                  <li>
-                    <Image src={icoFigma} alt="Figma" />
-                  </li>
-                  <li>
-                    <Image src={icoPhotoshop} alt="Photoshop" />
-                  </li>
-                  <li>
-                    <Image src={icoIllustrator} alt="Illustrator" />
-                  </li>
-                </ul>
-                <hr/>
-                <Link href="https://mi-banco.claudiosalazar.cl" target='new' className="link-portafolio-proyectos">
-                  <span className='ico-mas-proyecto'></span>
-                  <span className='texto-link-portafolio'>Ver proyecto</span>
-                </Link>
-              </div>
-            </div>
+    <>
+      <section className="portafolio row d-flex align-items-center pb-md-5">
+        <motion.div variants={titulo} initial='hidden' animate={controlTitulo} className='titulo col-11 ol-xl-10 mx-auto text-center pt-4 d-flex flex-column flex-md-row align-items-md-center'>
+          <h1 className='text-center text-md-end mb-4'>
+            <Link href='/portafolio' className="titulo-seccion d-flex align-items-center justify-content-center justify-content-md-end mb-3 mb-md-0">
+              <span className="ico-atras d-block"></span>
+              <span className=" d-block">Portafolio</span>
+            </Link>
+            <span className="subtitulo-seccion">Desarrollo</span>
+          </h1>
+          <div className="detalle-seccion">
+            <p className="parrafo-seccion">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ullamcorper erat eu ipsum bibendum cursus. Sed mauris arcu, tincidunt ut egestas sit amet, finibus a lacus. Morbi venenatis, tellus ac pharetra tempor, neque quam lacinia dolor, et luctus tellus dui ac urna.
+            </p>
           </div>
-          <div className="col-12 detalle-proyecto">
-            <div className="row">
-              <div className="col-12 col-md-4">
-                <Image src={miBanco2} alt="Mi Banco" className="img-fluid" />
-              </div>
-              <div className="col-12 col-md-8">
-                <h2>Mi Banco</h2>
-                <p>
-                  Proyecto personal de sitio bancario ficticio. Creación de diseño UI y desarrollo frontend y backend (Node.js). En el se pueden realizar transferencias, pagos de productos y modificación de datos del usuario con se ingresa al sitio. Los datos para poder ingresar y visualizar el proyecto son:
-                </p>
-                <p>
-                  <span>Usuario: <strong>12.840.432-5</strong></span>
-                  <br/>
-                  <span>Clave: <strong>Usuario-123</strong></span>
-                </p>
-                <p>
-                  <span>Usuario: <strong>9.328.275-2</strong></span>
-                  <br/>
-                  <span>Clave: <strong>Usuario-123</strong></span>
-                </p>
-                <p>Tecnologías y software utilizados:</p>
-                <ul className="tecnologias">
-                  <li>
-                    <Image src={icoAngular} alt="Angular" />
-                  </li>
-                  <li>
-                    <Image src={icoNode} alt="Nodejs" />
-                  </li>
-                  <li>
-                    <Image src={icoMysql} alt="MySQL" />
-                  </li>
-                  <li>
-                    <Image src={icoHtml} alt="HTML" />
-                  </li>
-                  <li>
-                    <Image src={icoSass} alt="Sass" />
-                  </li>
-                  <li>
-                    <Image src={icoBootstrap} alt="Bootstrap 5" />
-                  </li>
-                  <li>
-                    <Image src={icoFigma} alt="Figma" />
-                  </li>
-                  <li>
-                    <Image src={icoPhotoshop} alt="Photoshop" />
-                  </li>
-                  <li>
-                    <Image src={icoIllustrator} alt="Illustrator" />
-                  </li>
-                </ul>
-                <hr/>
-                <Link href="https://mi-banco.claudiosalazar.cl" target='new' className="link-portafolio-proyectos">
-                  <span className='ico-mas-proyecto'></span>
-                  <span className='texto-link-portafolio'>Ver proyecto</span>
-                </Link>
-              </div>
-            </div>
-          </div>
-          <div className="col-12 detalle-proyecto">
-            <div className="row">
-              <div className="col-12 col-md-4">
-                <Image src={miBanco3} alt="Mi Banco" className="img-fluid" />
-              </div>
-              <div className="col-12 col-md-8">
-                <h2>Mi Banco</h2>
-                <p>
-                  Proyecto personal de sitio bancario ficticio. Creación de diseño UI y desarrollo frontend y backend (Node.js). En el se pueden realizar transferencias, pagos de productos y modificación de datos del usuario con se ingresa al sitio. Los datos para poder ingresar y visualizar el proyecto son:
-                </p>
-                <p>
-                  <span>Usuario: <strong>12.840.432-5</strong></span>
-                  <br/>
-                  <span>Clave: <strong>Usuario-123</strong></span>
-                </p>
-                <p>
-                  <span>Usuario: <strong>9.328.275-2</strong></span>
-                  <br/>
-                  <span>Clave: <strong>Usuario-123</strong></span>
-                </p>
-                <p>Tecnologías y software utilizados:</p>
-                <ul className="tecnologias">
-                  <li>
-                    <Image src={icoAngular} alt="Angular" />
-                  </li>
-                  <li>
-                    <Image src={icoNode} alt="Nodejs" />
-                  </li>
-                  <li>
-                    <Image src={icoMysql} alt="MySQL" />
-                  </li>
-                  <li>
-                    <Image src={icoHtml} alt="HTML" />
-                  </li>
-                  <li>
-                    <Image src={icoSass} alt="Sass" />
-                  </li>
-                  <li>
-                    <Image src={icoBootstrap} alt="Bootstrap 5" />
-                  </li>
-                  <li>
-                    <Image src={icoFigma} alt="Figma" />
-                  </li>
-                  <li>
-                    <Image src={icoPhotoshop} alt="Photoshop" />
-                  </li>
-                  <li>
-                    <Image src={icoIllustrator} alt="Illustrator" />
-                  </li>
-                </ul>
-                <hr/>
-                <Link href="https://mi-banco.claudiosalazar.cl" target='new' className="link-portafolio-proyectos">
-                  <span className='ico-mas-proyecto'></span>
-                  <span className='texto-link-portafolio'>Ver proyecto</span>
-                </Link>
-              </div>
-            </div>
-          </div>
-          
-        </div>
-      </motion.div>
+        </motion.div>
+      </section>
       
-    </section>
+      <motion.div variants={contenidoPortafolio} initial='hidden' animate={controlContenido} className='contenidoPortafolio container-fluid proyectos-con-descripcion'>
+        <div className="row">
+          <div className="col-12">
+            <div className="row">
+
+              <div className="col-12 p-4 p-md-5 detalle-proyecto">
+                <div className="row d-lg-flex justify-content-xxl-center">
+                  <div className="col-12 col-md-6 col-xxl-4">
+                    <Image src={miBanco1} alt="Mi Banco" className="img-fluid pe-md-4" />
+                  </div>
+                  <div className="col-12 col-md-6 col-xxl-6 ps-md-4 descripcion-proyecto">
+                    <h2 className="mt-4 mt-md-0">Mi Banco</h2>
+                    <p>
+                      Proyecto personal de sitio bancario ficticio. Creación de diseño UI y desarrollo frontend y backend (Node.js). En el se pueden realizar transferencias, pagos de productos y modificación de datos del usuario con se ingresa al sitio. Los datos para poder ingresar y visualizar el proyecto son:
+                    </p>
+                    <p>
+                      <span>Usuario: <strong>12.840.432-5</strong></span>
+                      <br/>
+                      <span>Clave: <strong>Usuario-123</strong></span>
+                    </p>
+                    <p>
+                      <span>Usuario: <strong>9.328.275-2</strong></span>
+                      <br/>
+                      <span>Clave: <strong>Usuario-123</strong></span>
+                    </p>
+                    <p>Tecnologías y software utilizados:</p>
+                    <ul className="tecnologias">
+                      <li>
+                        <Image src={icoAngular} alt="Angular" />
+                      </li>
+                      <li>
+                        <Image src={icoNode} alt="Nodejs" />
+                      </li>
+                      <li>
+                        <Image src={icoMysql} alt="MySQL" />
+                      </li>
+                      <li>
+                        <Image src={icoHtml} alt="HTML" />
+                      </li>
+                      <li>
+                        <Image src={icoSass} alt="Sass" />
+                      </li>
+                      <li>
+                        <Image src={icoBootstrap} alt="Bootstrap 5" />
+                      </li>
+                      <li>
+                        <Image src={icoFigma} alt="Figma" />
+                      </li>
+                      <li>
+                        <Image src={icoPhotoshop} alt="Photoshop" />
+                      </li>
+                      <li>
+                        <Image src={icoIllustrator} alt="Illustrator" />
+                      </li>
+                    </ul>
+                    <hr/>
+                    <Link href="https://mi-banco.claudiosalazar.cl" target='new' className="link-portafolio-proyectos">
+                      <span className='ico-mas-proyecto'></span>
+                      <span className='texto-link-portafolio'>Ver proyecto</span>
+                    </Link>
+                  </div>
+                </div>
+              </div>
+              
+              <div className="col-12 p-4 p-md-5 detalle-proyecto">
+                <div className="row d-lg-flex justify-content-xxl-center">
+                  <div className="col-12 col-md-6 col-xxl-4">
+                    <Image src={miBanco2} alt="Mi Banco" className="img-fluid pe-md-4" />
+                  </div>
+                  <div className="col-12 col-md-6 col-xxl-6 ps-md-4 descripcion-proyecto">
+                    <h2 className="mt-4 mt-md-0">Mi Banco</h2>
+                    <p>
+                      Proyecto personal de sitio bancario ficticio. Creación de diseño UI y desarrollo frontend y backend (Node.js). En el se pueden realizar transferencias, pagos de productos y modificación de datos del usuario con se ingresa al sitio. Los datos para poder ingresar y visualizar el proyecto son:
+                    </p>
+                    <p>
+                      <span>Usuario: <strong>12.840.432-5</strong></span>
+                      <br/>
+                      <span>Clave: <strong>Usuario-123</strong></span>
+                    </p>
+                    <p>
+                      <span>Usuario: <strong>9.328.275-2</strong></span>
+                      <br/>
+                      <span>Clave: <strong>Usuario-123</strong></span>
+                    </p>
+                    <hr/>
+                    <Link href="https://mi-banco.claudiosalazar.cl" target='new' className="link-portafolio-proyectos">
+                      <span className='ico-mas-proyecto'></span>
+                      <span className='texto-link-portafolio'>Ver proyecto</span>
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </motion.div>
+    </>
   );
  }
 
--- src/app/portafolio/diseno-ui/page.tsx
+++ src/app/portafolio/diseno-ui/page.tsx
@@ -2,7 +2,11 @@
 
 import { easeIn, motion, useAnimation } from "framer-motion";
 import Link from "next/link";
+import Image from 'next/image';
 import { useEffect } from "react";
+
+import cym from '../../assets/portafolio/disenho/cym.jpg';
+import loveRockBeer from '../../assets/portafolio/disenho/love-rock-beer.jpg';
 
  function Ui () {
   const controlTitulo = useAnimation();
@@ -54,22 +58,63 @@ import { useEffect } from "react";
   }, [controlTitulo, controlContenido]);
   
   return (
-    <section className="portafolio row d-flex align-items-center">
-      <motion.div variants={titulo} initial='hidden' animate={controlTitulo} className='titulo col-11 ol-xl-10 mx-auto text-center pt-4 d-flex flex-column flex-md-row align-items-md-center'>
-        <h1 className='text-center text-md-end mb-4'>
-          <Link href='/portafolio' className="titulo-seccion d-flex align-items-center justify-content-center justify-content-md-end mb-3 mb-md-0">
-            <span className="ico-atras d-block"></span>
-            <span className=" d-block">Portafolio</span>
-          </Link>
-          <span className="subtitulo-seccion">Diseño UI</span>
-        </h1>
-        <div className="detalle-seccion">
-          <p className="parrafo-seccion">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ullamcorper erat eu ipsum bibendum cursus. Sed mauris arcu, tincidunt ut egestas sit amet, finibus a lacus. Morbi venenatis, tellus ac pharetra tempor, neque quam lacinia dolor, et luctus tellus dui ac urna.
-          </p>
+    <>
+      <section className="portafolio row d-flex align-items-center">
+        <motion.div variants={titulo} initial='hidden' animate={controlTitulo} className='titulo col-11 ol-xl-10 mx-auto text-center pt-4 d-flex flex-column flex-md-row align-items-md-center'>
+          <h1 className='text-center text-md-end mb-4'>
+            <Link href='/portafolio' className="titulo-seccion d-flex align-items-center justify-content-center justify-content-md-end mb-3 mb-md-0">
+              <span className="ico-atras d-block"></span>
+              <span className=" d-block">Portafolio</span>
+            </Link>
+            <span className="subtitulo-seccion d-block">Diseño UI</span>
+          </h1>
+          <div className="detalle-seccion">
+            <p className="parrafo-seccion">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ullamcorper erat eu ipsum bibendum cursus. Sed mauris arcu, tincidunt ut egestas sit amet, finibus a lacus. Morbi venenatis, tellus ac pharetra tempor, neque quam lacinia dolor, et luctus tellus dui ac urna.
+            </p>
+          </div>
+        </motion.div>
+      </section>
+
+      <motion.div variants={contenidoPortafolio} initial='hidden' animate={controlContenido} className='contenidoPortafolio container-fluid proyectos-con-descripcion'>
+        <div className="row">
+          <div className="col-12">
+            <div className="row">
+
+              <div className="col-12 p-4 p-md-5 detalle-proyecto">
+                <div className="row d-lg-flex justify-content-xxl-center">
+                  <div className="col-12 col-md-8 col-xxl-7">
+                    <Image src={cym} alt="Mi Banco" className="img-fluid pe-md-4 diseno-thumbnail" />
+                  </div>
+                  <div className="col-12 col-12 col-md-4 col-xxl-3 ps-md-4 descripcion-proyecto">
+                    <h2 className="mt-4 mt-md-0">C&M Consultores</h2>
+                    <p><strong>Tipo proyecto:</strong></p>
+                    <p>Imagen corporativa</p>
+                    <p>
+                      Proyecto personal de sitio bancario ficticio. Creación de diseño UI y desarrollo frontend y backend (Node.js). En el se pueden realizar transferencias, pagos de productos y modificación de datos del usuario con se ingresa al sitio. Los datos para poder ingresar y visualizar el proyecto son:
+                    </p>
+                  </div>
+                </div>
+              </div>
+              
+              <div className="col-12 p-4 p-md-5 detalle-proyecto mb-0">
+                <div className="row d-lg-flex justify-content-xxl-center">
+                  <div className="col-12 col-md-8 col-xxl-7">
+                    <Image src={loveRockBeer} alt="Mi Banco" className="img-fluid pe-md-4 diseno-thumbnail" />
+                  </div>
+                  <div className="col-12 col-12 col-md-4 col-xxl-3 ps-md-4 descripcion-proyecto">
+                    <h2 className="mt-4 mt-md-0">Mi Banco</h2>
+                    <p>
+                      Proyecto personal de sitio bancario ficticio. Creación de diseño UI y desarrollo frontend y backend (Node.js). En el se pueden realizar transferencias, pagos de productos y modificación de datos del usuario con se ingresa al sitio. Los datos para poder ingresar y visualizar el proyecto son:
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </motion.div>
-    </section>
+    </>
   );
  }
 
