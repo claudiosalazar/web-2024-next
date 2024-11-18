10d20f5f3a828be702ee4309fcffef66a3a62cda
Claudio Salazar <contacto@claudiosalazar.cl>
2024-11-18 06:09:08 -0300

    Ajuste en next.config para compilar proyecto para produccion

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',
+  distDir: 'prod',
+  basePath: '',
+  assetPrefix: 'https://www.claudiosalazar.cl/',
+};
 
 export default nextConfig;
