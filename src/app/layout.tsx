156ca88f50c26eb2b0d91ed19bcecdcf072a75ab
Claudio Salazar <contacto@claudiosalazar.cl>
2024-11-26 07:55:24 -0300

    Se agrego script para estadisticas de google analitycs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,6 +20,22 @@ function RootLayout({
 }>) {
   return (
     <html lang="es">
+      <head>
+        {/* Google Analytics */}
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=G-XZWXKEK7HB`}
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-XZWXKEK7HB');
+          `}
+        </Script>
+      </head>
       <body>
         <div className="contenido">
           <Header />
